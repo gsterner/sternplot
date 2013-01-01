017ca40216f7ac3b42b2593792a65d5a720af9c7
Gustaf Sterner <gsterner@gmail.com>
2013-01-01 21:30:55 +0100

    Canvas is now cleared before each plot.

--- plotmain.js
+++ plotmain.js
@@ -26,11 +26,12 @@ function addExpression(ctx, expr, texty) {
 function drawLine(ctx, x_values, y_values) {
     ctx.strokeStyle = '#00f';
     ctx.lineWidth   = 2;
+    ctx.beginPath();
     ctx.moveTo(x_values[0],y_values[0]);
     for ( i = 1; i < x_values.length; i++) {
         ctx.lineTo(x_values[i],y_values[i]);
     }
-    ctx.stroke()
+    ctx.stroke();
 }
 
 function drawOnCanvas(xArray, yArray, expr) {
@@ -38,12 +39,13 @@ function drawOnCanvas(xArray, yArray, expr) {
     var ctx=canvas.getContext("2d");
     var canvas_width = canvas.width;
     var canvas_height = canvas.height;
-    var frame_width = canvas.width * 0.1
-    var frame_height = canvas.height * 0.1
+    var frame_width = canvas.width * 0.1;
+    var frame_height = canvas.height * 0.1;
 
     var x_values = transformArrayToCoord(xArray, canvas_width, transformX, frame_width, frame_width)
     var y_values = transformArrayToCoord(yArray, canvas_height, transformY, frame_height, frame_height)
 
+    ctx.clearRect(0, 0, canvas_width, canvas_width);	
     addExpression(ctx, expr, canvas_height * 0.05)	
     drawFrame(ctx, canvas_width, canvas_height, canvas_width * 0.1, canvas_height * 0.1)
     drawLine(ctx, x_values, y_values)
